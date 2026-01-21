fbcd9ceb53c0a55c3cb203fffada7715e0a563e0
Omer Eizenberg <omer.eizenberg@gmail.com>
2026-01-21 21:11:44 +0200

    Calander progress

--- Assets/Scripts/Lobby/MonthlyChallengeController.cs
+++ Assets/Scripts/Lobby/MonthlyChallengeController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System;
+using UnityEditor.UI;
 
 
 public class MonthlyChallengeController : MonoBehaviour
@@ -11,14 +12,32 @@ public class MonthlyChallengeController : MonoBehaviour
     [Header("UI References")]
     [Tooltip("Assign all 35 day images here (Row 1 Col 1 to Row 5 Col 7)")]
     [SerializeField] private Image[] dayImages;
+    [SerializeField] private Color m_NormalColor;
+    [SerializeField] private Color m_PassedColor;
+    [SerializeField] private Color m_TodayColor;
+
+    [SerializeField] private Color m_NormalColorTxt;
+    [SerializeField] private Color m_PassedColorTxt;
+    [SerializeField] private Color m_TodayColorTxt;
     
     [Tooltip("Assign all 35 day text components here")]
     [SerializeField] private TextMeshProUGUI[] dayTexts;
 
+    public Image m_SelectedDateBg;
+    public TextMeshProUGUI m_SelectedDateTxt;
+
     private void OnEnable()
     {
+        m_NormalColor.a = 256f;
+        m_PassedColor.a = 256f;
+        m_TodayColor.a = 256f;
+        m_NormalColorTxt.a = 256f;
+        m_PassedColorTxt.a = 256f;
+        m_TodayColorTxt.a = 256f;
+
         // Automatically initialize to the current month and year
         Init(DateTime.Now.Month, DateTime.Now.Year);
+        MarkAsPassed();
     }
 
     public void Init(int month, int year)
@@ -45,6 +64,8 @@ public class MonthlyChallengeController : MonoBehaviour
                 // Active day
                 dayImages[i].gameObject.SetActive(true);
                 dayTexts[i].text = dateNumber.ToString();
+                dayImages[i].color = m_NormalColor;
+                dayTexts[i].color = m_NormalColorTxt;
             }
             else
             {
@@ -52,5 +73,18 @@ public class MonthlyChallengeController : MonoBehaviour
                 dayImages[i].gameObject.SetActive(false);
             }
         }
+    }
+
+    public void MarkAsSelected(Image i_bgDate)
+    {
+        m_SelectedDateBg = i_bgDate;
+        m_SelectedDateTxt = m_SelectedDateBg.GetComponentInChildren<TextMeshProUGUI>();;
+    MarkAsPassed();
+    }
+
+   public void MarkAsPassed()
+    {
+        m_SelectedDateBg.color = m_PassedColor;
+        m_SelectedDateTxt.color = m_PassedColorTxt;
     }
 }
