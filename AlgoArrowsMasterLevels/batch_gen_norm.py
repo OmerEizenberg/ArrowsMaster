bdccecba714fa6e7fcb69a1314a2d245a7c3b22e
Omer Eizenberg <omer.eizenberg@gmail.com>
2026-02-20 08:00:25 +0200

    Update challenge levels

--- AlgoArrowsMasterLevels/batch_gen_norm.py
+++ AlgoArrowsMasterLevels/batch_gen_norm.py
@@ -49,7 +49,7 @@ COMMON_CONFIG = {
     "ALPHA_THRESHOLD": 128
 }
 
-DURATION_MULTIPLIER = 0.121
+DURATION_MULTIPLIER = 0.24
 
 def rgb_to_hex(rgb):
     return '#{:02x}{:02x}{:02x}'.format(rgb[0], rgb[1], rgb[2])
